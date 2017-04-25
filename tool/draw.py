27755cc45f42aab3b758f324d38dc4795b76e049
xuming <507153809@qq.com>
2017-04-25 14:38:03 +0800

    update draw.py.xuming 20170425

--- tool/draw.py
+++ tool/draw.py
@@ -3,9 +3,10 @@
 @description: 
 @author:XuMing
 """
-import numpy as np
-import matplotlib.pyplot as plt
 import time
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 # plt.ion() #开启interactive mode
 x = np.linspace(0, 50, 1000)
@@ -17,5 +18,5 @@ plt.close(1)
 plt.figure(2)  # 创建图表2
 plt.plot(x, np.cos(x))
 plt.draw()
-time.sleep(2)
-print 'it is ok'
+time.sleep(1)
+print('it is ok')
