cc8cdf338df98861c7624e4e56194b5a9ea4c209
xuming06 <507153809@qq.com>
2018-05-03 17:48:11 +0800

    add xlearn demo.

---
+++ 21xlearn/xlearn_demo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Author: XuMing <xuming624@qq.com>
+# Brief: 
+import xlearn as xl
+
+# Training task
+ffm_model = xl.create_ffm() # Use field-aware factorization machine
+ffm_model.setTrain("../data/xlearn_data/small_train.txt")  # Training data
+ffm_model.setValidate("../data/xlearn_data/small_test.txt")  # Validation data
+
+# param:
+#  0. binary classification
+#  1. learning rate: 0.2
+#  2. regular lambda: 0.002
+#  3. evaluation metric: accuracy
+param = {'task':'binary', 'lr':0.2,
+         'lambda':0.002, 'metric':'acc'}
+
+# Start to train
+# The trained model will be stored in model.out
+ffm_model.fit(param, './model.out')
+
+# Prediction task
+ffm_model.setTest("../data/xlearn_data/small_test.txt")  # Test data
+ffm_model.setSigmoid()  # Convert output to 0-1
+
+# Start to predict
+# The output result will be stored in output.txt
+ffm_model.predict("./model.out", "./output.txt")
