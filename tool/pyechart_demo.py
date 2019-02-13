27903f1ca94b8a97ffb6eba2d1ea95b3e879853f
xuming06 <507153809@qq.com>
2019-02-13 22:50:38 +0800

    add pyechart.

---
+++ tool/pyechart_demo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+
+from pyecharts import Bar
+
+bar = Bar("我的第一个图表", "这里是副标题")
+bar.add("服装", ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"], [5, 20, 36, 10, 75, 90],is_more_utils=True)
+# bar.print_echarts_options() # 该行只为了打印配置项，方便调试时使用
+bar.render()    # 生成本地 HTML 文件
