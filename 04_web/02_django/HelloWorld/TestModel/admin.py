5552d59bb14a0dec45bd714f80b1ebdc54b9b874
shibing624 <shibing624@126.com>
2021-09-02 10:07:11 +0800

    update web.

--- 04_web/02_django/HelloWorld/TestModel/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from TestModel.models import RiskData,Contact, Tag
-
-admin.site.register(RiskData)
-admin.site.register(Tag)
-admin.site.register(Contact)
--- 04_web/02_django/HelloWorld/TestModel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestmodelConfig(AppConfig):
-    name = 'TestModel'
